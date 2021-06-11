a129b8c323f78fdb39c25d42fefa0b4ca15053a8
YuudachiXMMY <934647325@qq.com>
2021-06-11 16:00:15 +0800

    try to work out the basic structure

--- BenchmarkAutomation.py
+++ BenchmarkAutomation.py
@@ -1,10 +1,39 @@
+import Game
+
 class BenchmarkAutomation:
     '''
     '''
 
-    def __init__(self) -> None:
+    def __init__(self, steamDirectory="", documentDirectory="") -> None:
         '''
         '''
         pass
 
-    pass
+    def addGame(self, exe="", relativePath="", absolutePath=""):
+        '''
+        '''
+        return Game(self.getSteamDirectory, self.getDocumentDirectory(), \
+            exe, relativePath, absolutePath)
+
+
+    ############################################################################
+    def addSteamDirectory(self, dir):
+        self.steamDirectory = dir
+
+    def changeSteamDirectory(self, dir):
+        self.addSteamDirectory(dir)
+
+    def getSteamDirectory(self):
+        return self.steamDirectory
+
+    def addDocumentDirectory(self, dir):
+        self.documentDirectory = dir
+
+    def changeDocumentDirectory(self, dir):
+        self.addSteamDirectory(dir)
+
+    def getDocumentDirectory(self):
+        return self.documentDirectory
+
+
+    ############################################################################
---
+++ Game.py
@@ -0,0 +1,9 @@
+class Game:
+    '''
+    '''
+
+    def __init__(self, steamDirectory="", documentDirectory="", \
+        exe="", relativePath="", absolutePath="") -> None:
+        '''
+        '''
+        pass
